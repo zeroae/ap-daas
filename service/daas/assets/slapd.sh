7e72ebe37e41ecf2a31cba6d69898e3fe21a6017
Patrick Sodré <sodre@sodre.co>
2017-03-02 04:25:22 -0500

    Ensure /var/run/slapd exists before starting slapd

--- service/daas/assets/slapd.sh
+++ service/daas/assets/slapd.sh
@@ -2,4 +2,5 @@
 
 ulimit -n 1024
 export KRB5_KTNAME=/etc/ldap/sasl2/krb5.keytab
+[ -d /var/run/slapd ] || mkdir -p /var/run/slapd && chown -R openldap:openldap /var/run/slapd
 slapd -h "ldap:// ldapi://" -u openldap -g openldap $@
